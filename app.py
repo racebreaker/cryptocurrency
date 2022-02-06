357cb87f2e8be4192947b90554e7173d1e3fa755
racebreaker <92322521+racebreaker@users.noreply.github.com>
2022-02-06 16:28:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # app.py
 import app1
 import app8
+import app9
 import notes
 
 import streamlit as st
 PAGES = {
     "Algorithmic Trading": app8,
+    "Company Valuation (DCF)": app9,
     "Release Notes": notes,
 }
 st.sidebar.title('Navigation')
